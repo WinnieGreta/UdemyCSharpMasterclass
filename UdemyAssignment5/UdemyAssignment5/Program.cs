4ab7cb45600b3aea1a98fb4cb533b0ef974cba11
trush <trushchakolga@gmail.com>
2022-12-08 13:06:19 +0200

    add assignment 5

---
+++ UdemyAssignment5/UdemyAssignment5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UdemyAssignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name1 = "Vova";
+            string name2 = "Dasha";
+            string name3 = "Anya";
+
+            GreetFriend(name1);
+            GreetFriend(name2);
+            GreetFriend(name3);
+        }
+
+        public static void GreetFriend(string name)
+        {
+            Console.WriteLine($"Hi {name}, my friend!");
+        }
+    }
+}
