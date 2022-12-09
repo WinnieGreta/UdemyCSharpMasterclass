51f608d7e9f49aa024364e96484a7978113303c4
trush <trushchakolga@gmail.com>
2022-12-09 12:03:42 +0200

    add assignment 11

---
+++ UdemyAssignment11/UdemyAssignment11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace UdemyAssignment11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            analyzeTemp();
+        }
+
+        public static void analyzeTemp()
+        {
+            int temp;
+            string message;
+            Console.WriteLine("Enter the temperature in celcius:");
+            if (!int.TryParse(Console.ReadLine(), out temp))
+            {
+                Console.WriteLine("Not a valid temperature.");
+            } 
+            else
+            {
+                message = temp <= 15 ? "it's too cold here" : temp >= 16 && temp <= 28 ? "it is ok" : "it's hot here";
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
