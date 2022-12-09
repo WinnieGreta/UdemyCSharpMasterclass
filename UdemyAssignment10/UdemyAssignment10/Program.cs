cb7092d791f5727e539418d2fed2d7eef0a04f52
trush <trushchakolga@gmail.com>
2022-12-09 11:47:48 +0200

    add assignment 10

---
+++ UdemyAssignment10/UdemyAssignment10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UdemyAssignment10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temperature = -5;
+            FindState(temperature);
+
+            temperature += 10;
+            FindState(temperature);
+            
+            temperature += 100;
+            FindState(temperature);
+        }
+
+        public static void FindState(int temp)
+        {
+            string stateOfMatter;
+            stateOfMatter = temp > 100 ? "gas" : temp < 0 ? "solid" : "liquid";
+            Console.WriteLine(stateOfMatter);
+        }
+    }
+}
