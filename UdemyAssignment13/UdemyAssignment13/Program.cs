18875b106531f2fdc4c3985fb9808bc2684490c0
trush <trushchakolga@gmail.com>
2022-12-10 14:12:26 +0200

    add assignment 13

---
+++ UdemyAssignment13/UdemyAssignment13/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UdemyAssignment13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.Write("Count the people in the bus");
+            while(Console.ReadLine() == "")
+            {
+                Console.Write(count + 1);
+                count++;
+            }
+            Console.WriteLine("Okay, all {0} people in the bus", count);
+        }
+    }
+}
