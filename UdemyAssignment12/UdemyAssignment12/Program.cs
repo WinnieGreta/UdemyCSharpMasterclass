afb991f16151c943a37f7e1eb7103c0ac719c875
trush <trushchakolga@gmail.com>
2022-12-10 13:44:00 +0200

    add assignment 12

---
+++ UdemyAssignment12/UdemyAssignment12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UdemyAssignment12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine("Another way");
+
+            for (int i = 1; i < 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
