7c28cb10c2c1560413c51089d1e8c41fff599ede
trush <trushchakolga@gmail.com>
2022-12-11 16:24:39 +0200

    add assignment 18

---
+++ UnityAssignment18/UnityAssignment18/Box.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityAssignment18
+{
+    internal class Box
+    {
+        public Box()
+        {
+
+        }
+
+        public Box(int height, int width, int length)
+        {
+            Height = height;
+            Width = width;
+            Length = length;
+        }
+
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int Length { get; set; }
+
+        public int FrontSurface
+        {
+            get
+            {
+                return Height * Length;
+            }
+        }
+
+        public int Volume
+        {
+            get
+            {
+                return this.Height * this.Width * this.Length;
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Length is {0}, height is {1}, width is {2}, so the volume is {3}.", Length, Height, Width, Volume);
+        }
+    }
+}
---
+++ UnityAssignment18/UnityAssignment18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UnityAssignment18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box();
+            box1.Width = 5;
+            box1.Height = 2;
+            box1.Length = 3;
+
+            Console.WriteLine("Box 1 volume is " + box1.Volume);
+            Console.WriteLine("Front surface of box 1 is " + box1.FrontSurface);
+
+            box1.DisplayInfo();
+
+            Box box2 = new Box(10, 2, 7);
+            Console.WriteLine("Front surface of box 2 is " + box2.FrontSurface);
+            box2.DisplayInfo();
+        }
+    }
+}
