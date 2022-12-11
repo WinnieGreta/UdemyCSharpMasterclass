207b79b415ff73b1f7e6cb14cccbad5e322e5f55
trush <trushchakolga@gmail.com>
2022-12-11 14:40:15 +0200

    add assignment 17

---
+++ UdemyAssignment17/UdemyAssignment17/Human.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAssignment17
+{
+    internal class Human
+    {
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+        
+        public Human()
+        {
+            Console.WriteLine("Default human created");
+        }
+
+        public Human(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+        }
+
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public void IntroduceMyself()
+        {
+            if (firstName == null)
+            {
+                Console.Write("Hi, I'm default human");
+            }
+            else
+            {
+                Console.Write($"Hi, my name is {firstName}");
+                if (lastName != null)
+                {
+                    Console.Write($" {lastName}");
+                    if (eyeColor != null)
+                    {
+                        Console.Write($". My eye color is {eyeColor}");
+                        if (age != 0)
+                        {
+                            if (age == 1)
+                            {
+                                Console.Write(". I'm 1 year old");
+                            } 
+                            else
+                            {
+                                Console.Write($". I'm {age} years old");
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(".");
+        }
+    }
+}
---
+++ UdemyAssignment17/UdemyAssignment17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace UdemyAssignment17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human1 = new Human();
+            Human human2 = new Human("Alesia");
+            Human human3 = new Human("Anna", "Frank");
+            Human human4 = new Human("Franz", "Ferdiant", "brown");
+            Human human5 = new Human("Mark", "Brown", "blue", 1);
+            Human human6 = new Human("Jeff", "Bezos", "grey", 69);
+
+            human1.IntroduceMyself();
+            human2.IntroduceMyself();
+            human3.IntroduceMyself();
+            human4.IntroduceMyself();
+            human5.IntroduceMyself();
+            human6.IntroduceMyself();
+        }
+    }
+}
