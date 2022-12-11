f30e9ebcd4d662354bb5f420e15997eedad5ba93
trush <trushchakolga@gmail.com>
2022-12-11 12:29:03 +0200

    add assignment 15

---
+++ UdemyAssignment15/UdemyAssignment15/Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAssignment15
+{
+    internal class Human
+    {
+        public string firstName;
+        public string lastName;
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hello, my first name is {0} and my last name is {1}", firstName, lastName);
+        }
+    }
+}
---
+++ UdemyAssignment15/UdemyAssignment15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UdemyAssignment15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human arya = new Human();
+            arya.firstName = "Arya";
+            arya.lastName = "Stark";
+
+            Human vova = new Human();
+            vova.firstName = "Vova";
+            vova.lastName = "Crass";
+
+            arya.IntroduceMyself();
+            vova.IntroduceMyself();
+        }
+    }
+}
