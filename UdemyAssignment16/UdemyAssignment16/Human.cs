c99052b6f79a1a5feb960bff91b050bb433583d0
trush <trushchakolga@gmail.com>
2022-12-11 14:03:45 +0200

    add assignment 16

---
+++ UdemyAssignment16/UdemyAssignment16/Human.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAssignment16
+{
+    internal class Human
+    {
+        private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        public Human(string firstName, string lastName, string eyeColor, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0} {1}. I'm {2} years old and my eye color is {3}.", firstName, lastName, age, eyeColor);
+        }
+    }
+}
---
+++ UdemyAssignment16/UdemyAssignment16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UdemyAssignment16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human arya = new Human("Arya", "Stark", "grey", 3);
+            Human greta = new Human("Greta", "Winnie", "green", 26);
+
+            arya.IntroduceMyself();
+            greta.IntroduceMyself();
+        }
+    }
+}
