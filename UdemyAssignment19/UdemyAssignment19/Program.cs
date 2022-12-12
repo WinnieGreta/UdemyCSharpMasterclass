edc4b2e747ca17c3f5a4c40882bf01cca6374e1c
trush <trushchakolga@gmail.com>
2022-12-12 12:16:45 +0200

    add assignment 19

---
+++ UdemyAssignment19/UdemyAssignment19/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UdemyAssignment19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friends = new string[] {"Dasha", "Vova", "Anya", "Greta", "Hana"};
+
+            foreach (string friend in friends)
+            {
+                Console.WriteLine("Hello, " + friend + " my friend");
+            }
+        }
+    }
+}
