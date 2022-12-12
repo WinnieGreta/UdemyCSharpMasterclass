bdb157f0315ec5a273392c6083e67a2ee0aeaefe
trush <trushchakolga@gmail.com>
2022-12-12 14:38:37 +0200

    add assignment 22

---
+++ UdemyAssignment22/UdemyAssignment22/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace UdemyAssignment22
+{
+    internal class Program
+    {
+        static int[,] matrix =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        static void Main(string[] args)
+        {
+            PrintOddArray(matrix);
+       
+        }
+
+        public static void PrintOddArray(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if(arr[i,j] % 2 != 0)
+                    {
+                        Console.Write(arr[i,j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
