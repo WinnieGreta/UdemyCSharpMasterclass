079c8eb066da32d952222779cb90f72d68aead8c
trush <trushchakolga@gmail.com>
2022-12-12 14:45:48 +0200

    add assignment 23

---
+++ UdemyAssignment23/UdemyAssignment23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UdemyAssignment23
+{
+    internal class Program
+    {
+        static int[,] matrix =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if(i == j)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
