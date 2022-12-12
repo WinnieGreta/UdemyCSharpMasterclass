1e6179ea8d0726bfb53f17d351fa4932881e582f
trush <trushchakolga@gmail.com>
2022-12-12 15:16:38 +0200

    add assignment 24

---
+++ UdemyAssignment24/UdemyAssignment24/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UdemyAssignment24
+{
+    internal class Program
+    {
+        static int[,] matrix =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Diagonal elements:");
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.Write(matrix[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Negative diagonal elements:");
+            for(int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for(int j = matrix.GetLength(1) - 1; j >= 0 ; j--)
+                {
+                    if (i == matrix.GetLength(1) - j - 1)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
