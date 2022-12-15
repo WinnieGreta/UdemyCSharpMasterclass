b345cc222c3d7bff22260453c16da1472024500f
trush <trushchakolga@gmail.com>
2022-12-15 12:51:37 +0200

    add assignment 30

---
+++ UdemyAssignment30/UdemyAssignment30/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UdemyAssignment30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Convert(1));
+            Console.WriteLine(Convert(2));
+            Console.WriteLine(Convert(3));
+            Console.WriteLine(Convert(10));
+        }
+
+        public static string Convert(int i)
+        {
+            Dictionary<int, string> numbers = new Dictionary<int, string>()
+            {
+                { 0, "zero" },
+                { 1, "one" },
+                { 2, "two" },
+                { 3, "three" },
+                { 4, "four" },
+                { 5, "five" }
+            };
+
+            string number = null;
+
+            if (numbers.TryGetValue(i, out number))
+            {
+                return number;
+            }
+            else
+            {
+                return "nope";
+            }
+        }
+    }
+}
