f8119b54ac8408a9ab3725975732b6cc7f9fbfa7
trush <trushchakolga@gmail.com>
2022-12-15 14:33:09 +0200

    add assignment 31

---
+++ UdemyAssignment31/UdemyAssignment31/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UdemyAssignment31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(21);
+            stack.Push(32);
+            stack.Push(43);
+            stack.Push(54);
+
+            while (stack.Count > 0)
+            {
+                int val = stack.Pop();
+                Console.WriteLine(val);
+                Console.WriteLine($"Stack count is {stack.Count}");
+            }
+        }
+    }
+}
