0b7b10297529dac438695ca5071508a872100f1f
trush <trushchakolga@gmail.com>
2022-12-18 11:38:13 +0200

    add assignment 34

---
+++ UdemyAssignment34/UdemyAssignment34/Cat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyAssignment34
+{
+    internal class Cat : Animal
+    {
+        public bool IsHappy { get; set; }
+
+        public Cat(string name, int age) : base(name, age)
+        {
+            IsHappy = true;
+        }
+
+        public override void Eat()
+        {
+            base.Eat();
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meoww");
+        }
+
+        public override void Play()
+        {
+            if (IsHappy)
+            {
+                base.Play();
+            }
+        }
+    }
+}
---
+++ UdemyAssignment34/UdemyAssignment34/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UdemyAssignment34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Arya", 3);
+            Console.WriteLine($"This is my cat {cat.Name}. She's {cat.Age} years old.");
+            cat.Play();
+            cat.Eat();
+            cat.MakeSound();
+            cat.IsHungry = false;
+            cat.Eat();
+            cat.IsHappy = false;
+            cat.Play();
+            cat.IsHappy = true;
+        }
+    }
+}
