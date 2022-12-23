02c9dfcaf009b08bd0e68629b1f5180a90746093
trush <trushchakolga@gmail.com>
2022-12-23 11:21:35 +0200

    add assignment 41

---
+++ UdemyAssignment41/UdemyAssignment41/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace UdemyAssignment41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = File.ReadAllLines(@"D:\Users\trush\source\repos\UdemyCSharpMasterclass\UdemyAssignment41\Assets\input.txt");
+
+            foreach (string line in input)
+            {
+                if(line.Contains("split"))
+                {
+                    string[] splitResult = line.Split(' ');
+                    using(StreamWriter file = new StreamWriter(@"D:\Users\trush\source\repos\UdemyCSharpMasterclass\UdemyAssignment41\Assets\output.txt", true))
+                    {
+                        file.Write(splitResult[4] + " ");
+                    }
+                }
+            }
+        }
+    }
+}
