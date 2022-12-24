3a750c7cf512991ab2134beb490e2e46fa849093
trush <trushchakolga@gmail.com>
2022-12-24 12:57:11 +0200

    add assignment 43

---
+++ UdemyAssignment43/UdemyAssignment43/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UdemyAssignment43
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ask me anything!");
+            Console.ReadLine();
+            Answer();
+        }
+
+        private static void Answer()
+        {
+            Random ball = new Random();
+
+            int num = ball.Next(1, 4);
+
+            switch (num)
+            {
+                case 1: 
+                    Console.WriteLine("Yes");
+                    break;
+                case 2:
+                    Console.WriteLine("Maybe");
+                    break;
+                case 3:
+                    Console.WriteLine("No");
+                    break;
+            }
+        }
+    }
+}
