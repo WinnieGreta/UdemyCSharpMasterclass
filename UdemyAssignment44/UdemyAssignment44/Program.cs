fbcb3e9045784ed20bff6d07efe1244eefa47439
trush <trushchakolga@gmail.com>
2022-12-25 21:06:09 +0200

    add assignment 44

---
+++ UdemyAssignment44/UdemyAssignment44/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace UdemyAssignment44
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = File.ReadAllText(@"D:\Users\trush\source\repos\UdemyCSharpMasterclass\UdemyAssignment42\Assets\input2.txt");
+            string pattern = @"\d{2,3}";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(inputText);
+            foreach (Match match in matches)
+            {
+                int charValue = int.Parse(match.Value);
+                char letter = (char)charValue;
+                Console.WriteLine(letter);
+
+            }
+
+        }
+    }
+}
