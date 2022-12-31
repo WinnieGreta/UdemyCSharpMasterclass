31fa473542e25a5139a77df9e348686135401b2f
trush <trushchakolga@gmail.com>
2022-12-31 13:58:10 +0200

    add assignment 52

---
+++ UdemyAssignment52/UdemyAssignment52/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UdemyAssignment52
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Result resultObj = new Result() { Num1 = "1", Num2 = "3" };
+            //resultObj.Num1 = "1";
+            //resultObj.Num2 = "2";
+            this.DataContext = resultObj;
+        }
+    }
+}
---
+++ UdemyAssignment52/UdemyAssignment52/Result.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace UdemyAssignment52
+{
+    public class Result : INotifyPropertyChanged
+    {
+        private string? num1;
+        private string? num2;
+        private string sumResult;
+        private string multiResult;
+
+        public string Num1
+        {
+            get { return num1; }
+            set
+            {
+                int number;
+                bool res = int.TryParse(value, out number);
+                if (res) num1 = value;
+                OnPropertyChanged("Num1");
+                OnPropertyChanged("SumResult");
+                OnPropertyChanged("MultiResult");
+            }
+        }
+
+        public string Num2
+        {
+            get { return num2; }
+            set
+            {
+                int number;
+                bool res = int.TryParse(value, out number);
+                if (res) num2 = value;
+                OnPropertyChanged("Num2");
+                OnPropertyChanged("SumResult");
+                OnPropertyChanged("MultiResult");
+            }
+        }
+
+        public string SumResult
+        {
+            get
+            {
+                int res = int.Parse(Num1) + int.Parse(Num2);
+                return res.ToString();
+            }
+            set
+            {
+                int res = int.Parse(Num1) + int.Parse(Num2);
+                sumResult = res.ToString();
+                OnPropertyChanged("SumResult");
+            }
+        }
+
+        public string MultiResult
+        {
+            get
+            {
+                int res = int.Parse(Num1) * int.Parse(Num2);
+                return res.ToString();
+            }
+            set
+            {
+                int res = int.Parse(Num1) * int.Parse(Num2);
+                multiResult = res.ToString();
+                OnPropertyChanged("MultiResult");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged (string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged (this, new PropertyChangedEventArgs (property));
+            }
+        }
+    }
+}
